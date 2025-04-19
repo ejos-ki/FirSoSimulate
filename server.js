42d2d5a230b5699603ccef5ac89c7613ee2a9d4c
Jeo Latorre <latorrejeo1@gmail.com>
2025-04-19 08:20:38 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,3 @@
-// require('dotenv').config(); 
-// const { Vonage } = require('@vonage/server-sdk');
-
-// const vonage = new Vonage({
-//   apiKey: "596f115b",
-//   apiSecret: "r3b1QmUdBK2p1iTt"
-//   // apiKey: process.env.API_KEY,
-//   // apiSecret: process.env.API_SECRET
-// });
-
-// const from = "Vonage APIs"
-// // const to = "639217746831"
-// const to = process.env.PHONE_NUMBER;
-// // const to = "639618035299"
-// const text = 'Fire has been detected by your FirSo system at 2025-04-18 10:45 PM in your Living Room. Please check your property immediately and contact emergency services if necessary.'
-
-// async function sendSMS() {
-//     await vonage.sms.send({to, from, text})
-//         .then(resp => { console.log('Message sent successfully'); console.log(resp); })
-//         .catch(err => { console.log('There was an error sending the messages.'); console.error(err); });
-// }
-
-// sendSMS();
-
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
